4cf42aa93ef3f578d0166ff391f79a7534aa9269
zainmustafa <zmdhillo@gmail.com>
2017-03-19 12:15:47 +0500

    On Enter keypress add todo.

--- src/App.js
+++ src/App.js
@@ -18,44 +18,57 @@ class App extends Component {
         });
     }
 
-    addTodo (){
-
+    addTodo (event){
         this.state.todoList.push({todo:this.state.todo});
+
+        this.setState({
+            todo : '',
+            todoList : this.state.todoList
+        });
+        event.preventDefault();
+    }
+    deleteTodo(index,event){
+        delete this.state.todoList[index];
         this.setState({
             todoList : this.state.todoList
         })
-
     }
 
     getList(arr) {
         return (
             arr.map((item, index) => {
-                return <li key={index}>{item.todo}</li>
+                return <div key={index}>
+                            <li >{item.todo}</li>
+                            <button onClick={this.deleteTodo.bind(this,index)}>Delete</button>
+                        </div>
+
             })
         )
     }
+
   render() {
 
 
     return (
       <div className="App">
         <div className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-       <h2>Welcome to React </h2>
-       </div>
-       <p className="App-intro">
-       To get started, edit <code>src/App.js</code> and save to reload.
-       </p>
-       <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React </h2>
+        </div>
+        <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
            <ul>
                {this.getList(this.state.todoList)}
            </ul>
-
-           <input type="text" placeholder="Enter Todo" onChange={this.onChangeInput.bind(this)}/>
-
-
-           <button type="button" onClick={this.addTodo.bind(this)}>Add</button>
-       </div>
+            <form onSubmit={this.addTodo.bind(this)}>
+                <input type="text" placeholder="Enter Todo"
+                       onChange={this.onChangeInput.bind(this)}
+                       value={this.state.todo}/>
+                <button type="button" onClick={this.addTodo.bind(this)}>Add</button>
+            </form>
+        </div>
 
       </div>
     );
