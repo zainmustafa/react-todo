bba2f021b72c828067ceec82f67a3ae629d28127
zainmustafa <zmdhillo@gmail.com>
2017-03-19 13:43:17 +0500

    Content update

--- src/App.js
+++ src/App.js
@@ -63,13 +63,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React </h2>
+        <h2>React Todo</h2>
         </div>
         <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
+            React Todo App with basic features.
         </p>
         <div>
-           <table style={this.state.todoList.length !== 0 ? {} : { display: 'none' }}>
+           <table style={this.state.todoList.length === 0 ? { display: 'none' } : {} }>
                <thead>
                    <tr>
                        <th>Todo</th>
@@ -77,7 +77,6 @@ class App extends Component {
                        <th>Delete</th>
                    </tr>
                </thead>
-
                <tbody>
                    {this.getList(this.state.todoList)}
                </tbody>
